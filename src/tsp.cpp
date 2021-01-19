36673f60b8fca946a81e93af6e80eb8bfda1f7fc
Groupe4-algorithmique <75951199+Groupe4-algorithmique@users.noreply.github.com>
2021-01-19 22:53:30 +0100

    Update tsp.cpp

--- src/tsp.cpp
+++ src/tsp.cpp
@@ -14,8 +14,8 @@ using namespace Rcpp;
 //' G = G %*% t(G) # rendre G symétrique
 //' diag(G) = 0 # annuler la diagonale de G
 //' ## la symétrie de G n'est pas nécessaire.
-//' start_city = 1 # la ville de Départ.
-//' cities = c(2,3,4) # un ordre de ville à visiter.
+//' start_city = 0 # la ville de Départ.
+//' cities = c(1,2,3) # un ordre de ville à visiter.
 //' distance = compute_distance_Rcpp(G,cities,start_city)
 // [[Rcpp::export]] //mandatory to export the function
 double compute_distance_Rcpp(NumericMatrix G,
@@ -44,8 +44,8 @@ double compute_distance_Rcpp(NumericMatrix G,
 //' G = G %*% t(G) # rendre G symétrique (n'est pas nécessaire.)
 //' diag(G) = 0 # annuler la diagonale de G
 //' ## la symétrie de G n'est pas nécessaire.
-//' start_city = 1 # la ville de Départ.
-//' cities = c(2,3,4)  # un ordre de ville à visiter.
+//' start_city = 0 # la ville de Départ.
+//' cities = c(1,2,3)  # un ordre de ville à visiter.
 //' results = naive_method_Rcpp(G,cities,start_city)
 //' results['path_optimal'] # l'ordre des villes optimales
 //' results['dist_optimal'] # la distance obtenue avec l'ordre des villes optimales.
